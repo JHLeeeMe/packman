ce4483bd3d9a34255fdef13532914927b431c8b1
JHLeeeMe <lejung92@gmail.com>
2023-10-23 15:10:36 +0900

    [Update] for cross-platform
    - Update CMakeLists.txt
    - Update protocol structs in callback header for windows

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,8 +5,17 @@ int main(int argc, const char** argv)
 {
     Packman packman{ };
     packman.create_pcapd();
-    packman.set_filter_rule(argv[2]);
-    packman.start_loop(atoi(argv[1]), callback, nullptr);
+#if 0
+    {
+        packman.set_filter_rule(argv[2]);
+        packman.start_loop(atoi(argv[1]), callback, nullptr);
+    }
+#else
+    {
+        packman.set_filter_rule("tcp");
+        packman.start_loop(0, callback, nullptr);
+    }
+#endif
 
     return 0;
 }
