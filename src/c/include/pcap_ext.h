49b50811ca44ab5b7c2be614c2288b419f09fd41
JHLeeeMe <lejung92@gmail.com>
2023-10-23 15:04:43 +0900

    [Update] pch
    - Update pch code
    - Modify c code to linux-specific

--- src/c/include/pcap_ext.h
+++ src/c/include/pcap_ext.h
@@ -1,17 +1,6 @@
 #pragma once
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <linux/filter.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <netinet/ether.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
+#include "pch.h"
 
 struct PFHeader
 {
